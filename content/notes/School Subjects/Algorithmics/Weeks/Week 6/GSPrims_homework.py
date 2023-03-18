7463bc8827fa8d262223f9283f213b6ccb8fd500
Garv Shah <gshah.6110@gmail.com>
2023-03-18 13:46:29 +1100

    vault backup: 2023-03-18 13:46:29

--- content/notes/School Subjects/Algorithmics/Weeks/Week 6/GSPrims_homework.py
+++ content/notes/School Subjects/Algorithmics/Weeks/Week 6/GSPrims_homework.py
@@ -14,43 +14,50 @@ def start():
     canvas.label('title').remove()
     g = nx.Graph()
 
-    g.add_edge("A", "B", weight=8)
-    g.add_edge("B", "F", weight=17)
-    g.add_edge("F", "J", weight=24)
-    g.add_edge("J", "L", weight=14)
-    g.add_edge("L", "K", weight=15)
-    g.add_edge("K", "G", weight=19)
-    g.add_edge("G", "C", weight=20)
-    g.add_edge("C", "A", weight=13)
-    g.add_edge("C", "D", weight=12)
-    g.add_edge("B", "D", weight=14)
-    g.add_edge("B", "D", weight=14)
-    g.add_edge("D", "E", weight=3)
-    g.add_edge("E", "F", weight=7)
-    g.add_edge("E", "H", weight=16)
-    g.add_edge("G", "H", weight=8)
-    g.add_edge("H", "I", weight=5)
-    g.add_edge("K", "I", weight=17)
-    g.add_edge("I", "J", weight=7)
+    g.add_edge(1, 5, weight=3.2)
+    g.add_edge(4, 5, weight=3.5)
+    g.add_edge(4, 6, weight=9.3)
+    g.add_edge(3, 6, weight=5.2)
+    g.add_edge(3, 1, weight=2.9)
+    g.add_edge(7, 1, weight=1.9)
+    g.add_edge(2, 1, weight=3.6)
+    g.add_edge(5, 7, weight=2.8)
+    g.add_edge(2, 7, weight=3.4)
+    g.add_edge(2, 3, weight=1.7)
+    g.add_edge(7, 4, weight=3.7)
+    g.add_edge(0, 7, weight=1.6)
+    g.add_edge(0, 2, weight=2.6)
+    g.add_edge(0, 6, weight=5.8)
+    g.add_edge(0, 4, weight=3.8)
+    g.add_edge(2, 6, weight=4.0)
 
     # Render graph
     add_graph(canvas, g)
     canvas.nodes().color('gray')
     canvas.pause(1)
 
-    n = "A"
+    n = 1
     g.nodes[n]['seen'] = True
     seen = [n]
     canvas.node(n).color('dark-gray')
 
+    arr = []
+
     while len(seen) < len(g.nodes):
+        arr = []
         smallest = {"node": None, "weight": float('inf')}
         for node in seen:
             for n2 in g.neighbors(node):
                 weight = g.edges[node, n2]['weight']
+                if [(node, n2), weight] not in arr and [(n2, node), weight] not in arr and n2 not in seen:
+                    arr.append([(node, n2), weight])
                 if weight < smallest['weight'] and n2 not in seen:
                     smallest = {"node": n2, "weight": weight}
                     n = node
+
+        arr = sorted(arr, key=lambda x: x[1])
+        print(arr)
+        print('')
 
         canvas.edge((n, smallest['node'])).traverse('red').pause(0.5)
         n = smallest['node']
