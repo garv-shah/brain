c985bdb271eb2524bb783bf66f9a9e3eec3ecdf0
Garv Shah <64292655+garv-shah@users.noreply.github.com>
2023-04-26 23:06:14 +1000

    added more nodes and lines

--- content/notes/School Subjects/Algorithmics/Weeks/Week 10/School Graph/test.py
+++ content/notes/School Subjects/Algorithmics/Weeks/Week 10/School Graph/test.py
@@ -9,6 +9,7 @@ canvas = server.canvas()
 edges = [
     {'from': 'Glen Waverley', 'to': 'Caulfield Grammar - Wheelers Hill', 'weight': 16, 'line': '754 Bus Line'},
     {'from': 'Glen Waverley', 'to': 'Caulfield Grammar - Wheelers Hill', 'weight': 23, 'line': '753 Bus Line'},
+    {'from': 'Glen Waverley', 'to': 'Mount Waverley', 'weight': 12, 'line': '623 Bus Line'},
     {'from': 'Glen Waverley', 'to': 'Mount Waverley', 'weight': 8, 'line': 'Glen Waverley'},
     {'from': 'Mount Waverley', 'to': 'Richmond', 'weight': 20, 'line': 'Glen Waverley'},
     {'from': 'Richmond', 'to': 'Parliament', 'weight': 20, 'line': 'Glen Waverley'},
@@ -17,6 +18,21 @@ edges = [
     {'from': 'Flinders Street', 'to': 'Richmond', 'weight': 20, 'line': 'Glen Waverley'},
     {'from': 'Flinders Street', 'to': 'Brighton Beach', 'weight': 20, 'line': 'Sandringham'},
     {'from': 'Richmond', 'to': 'Camberwell', 'weight': 20, 'line': 'Camberwell'},
+    {'from': 'Oakleigh', 'to': 'Richmond', 'weight': 20, 'line': 'Pakenham'},
+    {'from': 'Richmond', 'to': 'Parliament', 'weight': 20, 'line': 'Pakenham'},
+    {'from': 'Parliament', 'to': 'Melbourne Central', 'weight': 20, 'line': 'Pakenham'},
+    {'from': 'Melbourne Central', 'to': 'Flinders Street', 'weight': 20, 'line': 'Pakenham'},
+    {'from': 'Flinders Street', 'to': 'Richmond', 'weight': 20, 'line': 'Pakenham'},
+    {'from': 'Caulfield Grammar - Wheelers Hill', 'to': 'Wheelers Hill Library', 'weight': 17, 'line': 'Walk'},
+    {'from': 'Wheelers Hill Library', 'to': 'Oakleigh', 'weight': 20, 'line': '693 Bus Line'},
+    {'from': 'Chadstone', 'to': 'Oakleigh', 'weight': 3, 'line': '800 Bus Line'},
+    {'from': 'Chadstone', 'to': 'Caulfield', 'weight': 11, 'line': '900 Bus Line'},
+    {'from': 'Caulfield', 'to': 'Flinders Street', 'weight': 16, 'line': 'Frankston'},
+    {'from': 'Caulfield', 'to': 'Oakleigh', 'weight': 16, 'line': '900 Bus Line'},
+    {'from': 'Caulfield', 'to': 'Oakleigh', 'weight': 27, 'line': 'Pakenham'},
+    {'from': 'Caulfield', 'to': 'Caulfield Grammar - Caulfield', 'weight': 15, 'line': 'Tram'},
+    {'from': 'Wheelers Hill Library', 'to': 'Brandon Park', 'weight': 6, 'line': '693 Bus Line'},
+    {'from': 'Oakleigh', 'to': 'Brandon Park', 'weight': 25, 'line': '693 Bus Line'},
 ]
 
 lineColours = {
@@ -25,6 +41,14 @@ lineColours = {
     'Camberwell': '#764193',
     '754 Bus Line': '#de852c',
     '753 Bus Line': '#7cde2c',
+    '623 Bus Line': '#3cbf6c',
+    '693 Bus Line': '#4cdf2c',
+    '800 Bus Line': '#4cdf2c',
+    '900 Bus Line': '#4cdf2c',
+    'Walk': '#4cdf2c',
+    'Tram': '#E285AF',
+    'Pakenham': '#3cbf6c',
+    'Frankston': '#3cbf6c',
 }
 
 
@@ -55,7 +79,7 @@ def start():
     for index in range(len(node_list)):
         node = node_list[index]
 
-        canvas.node(node).label('name').add(text=node)
+        canvas.node(node).label('name').add(text=node,color='black')
         canvas.node(node).label().add(text=str(index + 1))
 
 
